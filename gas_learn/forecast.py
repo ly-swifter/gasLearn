52271e8eb73cd740cc267c77c1c8065db69caac9
tianru996007 <78125629+tianru996007@users.noreply.github.com>
2021-02-03 19:11:14 +0800

    Print flag

--- gas_learn/forecast.py
+++ gas_learn/forecast.py
@@ -17,8 +17,8 @@ class Forecastting:
         sample_rate = pd.read_csv(SAMPLE_RATE_FILE)
         gas = pd.read_csv(file_path)
         flag='nick'
-        flag
-        gas.shape
+        print(flag)
+        print(gas.shape)
         gas = gas.iloc[len(gas) - 10000:len(gas), :]
         rate_all = [1.6180339887, 2.058, 2.6180339887, 3.33, 4.236]
         forecast_l_all = [
