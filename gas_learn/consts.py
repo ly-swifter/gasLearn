7eebc5e2bac3d2a31d5766296fb1165580332dcf
tianru996007 <78125629+tianru996007@users.noreply.github.com>
2021-02-04 19:06:14 +0800

    Update consts.py

--- gas_learn/consts.py
+++ gas_learn/consts.py
@@ -5,5 +5,6 @@ ORIGINAL_DATA_FILE = path.join(str(Path.home()), 'block_info.csv')
 ORIGINAL_TRAIN_DATA_FILE = path.join(str(Path.home()), 'block_new.csv')
 SAMPLE_RATE_FILE = path.join(str(Path.home()), 'sample_rate')
 L2LR_PICKLE_FILE = path.join(str(Path.home()), "L2LR.pickle")
+R_F = path.join(str(Path.home()), "range_forecast")
 
 TRAIN_RAW_RANG = 'TRAIN_RAW_RANG'
