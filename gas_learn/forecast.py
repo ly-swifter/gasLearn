985835c884e3fd6e48d4778285af3b8f045b3dca
tianru996007 <78125629+tianru996007@users.noreply.github.com>
2021-03-15 17:25:06 +0800

    Update forecast.py

--- gas_learn/forecast.py
+++ gas_learn/forecast.py
@@ -31,8 +31,7 @@ class Forecastting:
                 print('lost_input_data')
         gas = gas.drop(columns=['range', 'forecast'])
         gas = gas.sort_values(by=['epoch'])
-        gas = pd.merge(gas,  range_forecast), on='epoch',
-                       how='left').sort_values(by=['epoch'], ascending=True)
+        gas = pd.merge(gas, range_forecast, on='epoch', how='left').sort_values(by=['epoch'], ascending=True)
         gas = gas.iloc[len(gas) - 10000 : len(gas), :].fillna(0)
         rate_all = [1.6180339887, 2.058, 2.6180339887, 3.33, 4.236]
         forecast_l_all = [
