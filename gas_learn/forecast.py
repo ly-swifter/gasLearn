223951273f2a3f40540318644e74b7188c760497
tianru996007 <78125629+tianru996007@users.noreply.github.com>
2021-03-15 13:31:53 +0800

    Update forecast.py

--- gas_learn/forecast.py
+++ gas_learn/forecast.py
@@ -29,9 +29,9 @@ class Forecastting:
         if (gas.parent_basefee.iloc[len(gas) - 1] == 0):
             if (gas.parent_basefee.iloc[len(gas) - 2] == 0):
                 print('lost_input_data')
-        gas = gas.drop(columns=['range', 'forecast'])
+        gas = gas.drop(columns=['range', 'v'])
         gas = gas.sort_values(by=['epoch'])
-        gas = pd.merge(gas, range_forecast, on='epoch',
+        gas = pd.merge(gas, range_forecast.drop(columns = ['forecast120']), on='epoch',
                        how='left').sort_values(by=['epoch'], ascending=True)
         gas = gas.iloc[len(gas) - 10000 : len(gas), :].fillna(0)
         rate_all = [1.6180339887, 2.058, 2.6180339887, 3.33, 4.236]
@@ -326,5 +326,6 @@ class Forecastting:
         proba_res = proba_positive[0][0]
         if (proba_positive[0][0] < proba_positive[0][1]):
             proba_res = proba_positive[0][1]
-        print(is_increase, proba_positive, (0.5 - 1 / (1 + np.exp(proba_res - 0.5))) * 8.166 * forecast_res)
-        return is_increase, proba_positive, (0.5 - 1 / (1 + np.exp(proba_res - 0.5))) * 8.166 * forecast_res
+        
+        print(is_increase, proba_positive, (proba_res - 0.5) * forecast_res)
+        return is_increase, proba_positive, (proba_res - 0.5) * forecast_res
