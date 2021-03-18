ca68f96cabb05ecc33a8641c89e03739b8d1463f
tianru996007 <78125629+tianru996007@users.noreply.github.com>
2021-03-18 10:35:53 +0800

    Update forecast.py

--- gas_learn/forecast.py
+++ gas_learn/forecast.py
@@ -186,7 +186,7 @@ class Forecastting:
                 gas.forecast.iloc[len(gas) - 1] = forecast[i]
         range_forecast = pd.concat([epoch, gas.range.copy(), gas.forecast.copy()], axis=1)
         forecast_res_t = fee.copy().iloc[len(fee) - 240 : len(fee)].rolling(120).median() - fee.copy().iloc[len(fee) - 240 : len(fee)].shift(119)
-        forecast_res_t = forecast_res_t.iloc[len(forecast_res_t) - 120, len(forecast_res_t) :]
+        forecast_res_t = forecast_res_t.iloc[len(forecast_res_t) - 120 : len(forecast_res_t)]
         forecast_res = 0
         for i in range(len(forecast_res_t)):
             forecast_res =  forecast_res +  (i + 1) * forecast_res_t.iloc[i] / 60
