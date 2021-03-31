972a61fd7a5e0fd37a5ed5707afb100b3f82924c
tianru996007 <78125629+tianru996007@users.noreply.github.com>
2021-03-31 10:37:00 +0800

    Update forecast.py

--- gas_learn/forecast.py
+++ gas_learn/forecast.py
@@ -376,7 +376,7 @@ class Forecastting:
         forecast_m = 0
         for i in range(119):
             forecast_m = forecast_m + forecast_m_t.median()
-            forecast_m_t = forecast_m_t.iloc[1 : len(forecast_m_t) - 1]
+            forecast_m_t = forecast_m_t.iloc[1 : len(forecast_m_t)]
         forecast_m = forecast_m / 119
         print(is_increase, proba_positive, forecast_m - forecast_d, -forecast_d, forecast_t)
         return is_increase, proba_positive,  forecast_m - forecast_d
